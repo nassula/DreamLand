8c175aca564c8c87ab66d6abe42d2ed569880085
Diego Nassula <diegonassula@gmail.com>
2023-04-11 19:18:14 -0300

    Commit Inicial

---
+++ src/main/java/com/diegonassula/dreamland/DreamLandApplication.java
@@ -0,0 +1,13 @@
+package com.diegonassula.dreamland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamLandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamLandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diegonassula/dreamland/DreamLandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegonassula.dreamland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamLandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
